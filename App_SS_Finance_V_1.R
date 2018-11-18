c49ecc7dfe1153cf6ead77992aaa28161fa98f54
Carlos Flores Luna <Carlos.Flores@global.com>
2018-11-18 16:53:10 -0600

    Se dio formato a la tabla y se crearon los menus desplegables para categorias. Siguientes pasos, hacer que guarde los datos.

--- App_SS_Finance_V_1.R
+++ App_SS_Finance_V_1.R
@@ -39,22 +39,76 @@ header<- dashboardHeader()
 ########################
 #Definicion de Siderbar#
 ########################
-sidebar <- dashboardSidebar()
-
+sidebar <- dashboardSidebar(
+  sidebarMenu( #Funcion siderbarMenu; permite contener a las funciones menuItem, menuSubItem
+  menuItem("Carga de Datos", #Funcion menuItem; Genera los iconos en la barra lateral izquierda.
+           tabName = "Datos"),
+  menuItem("Graficas",
+           tabName = "dashboard"))
+  )
+  
+  
 ####################
 #Definicion de Body#
 ####################
   body <- dashboardBody(
-    helpText("Tabla Editable"),
+    #TabItems, nos permite especificar los objetos que se muestran en cada MenuItem
+    tabItems(
+    tabItem(tabName = "Datos",
+    #fluidrow, Permite almacenar varios objetos y especificar su tamaño y formato.        
+    fluidRow(
+    #Box1
+      box(
+      width = 3, title = "Formato de Carga Arvhivo",
+    fileInput(inputId = "Archivo", "Carga de Archivo",
+              multiple = TRUE,
+              accept = c("text/csv",
+                         "text/comma-separated-values,text/plain",
+                         ".csv")),
+    
+    # Linea horizontal
+    tags$hr(),
+    
+    # Input: Checkbox para encabezado
+    checkboxInput(inputId = "header", "Header", TRUE),
+    
+    # Input: Metodo de Separacion de los Datos
+    radioButtons(inputId = "sep", "Separacion",
+                 choices = c(Comma = ",",
+                             Semicolon = ";",
+                             Tab = "\t"),
+                 selected = ","),
+    
+    # Input: Select quotes ----
+    radioButtons(inputId = "quote", "Quote",
+                 choices = c(None = "",
+                             "Double Quote" = '"',
+                             "Single Quote" = "'"),
+                 selected = '"'),
+    
+    # Horizontal line ----
+    tags$hr(),
+    
+    # Input: Select number of rows to display ----
+    radioButtons(inputId = "disp", "Display",
+                 choices = c(Head = "head",
+                             All = "all"),
+                 selected = "head")
+    ),
+    #Box2
+  box(width = 8,title = "Tabla Editable",
     rHandsontableOutput(outputId = "Tabla"), #Muestra la Tabla
     br(),
     #Boton para Guardar los Resultados
     actionButton(inputId = "Btn_Guardar","Guardar Cambios")
   )
+    )
+      ),
+  #MenuItem, dashboard
+  tabItem(tabName = "dashboard")
+  )
+    )
   
-  
-  
-
 # Funcion dashboadPage; Crea una pagina de panel de control para usar una aplicacion Shiny
   ui <- dashboardPage(header = header,
                       sidebar = sidebar,
@@ -67,11 +121,38 @@ sidebar <- dashboardSidebar()
 server <- function(input, output) {
   #Funcion render conectada con rHandsontableOutput
   output$Tabla <- renderRHandsontable({
-    rhandsontable(head(iris))
+    
+    req(input$Archivo)
+    
+  df <- read.csv(input$Archivo$datapath,
+                 header = input$header,
+                 sep = input$sep,
+                 quote = input$quote,
+                 stringsAsFactors = FALSE)
+  
+  df$categoria <- as.factor("-")
+  
+    if(input$disp == "head"){
+      return(head(rhandsontable(df, width = 800,height = 300) %>% #Funcion rhandsontable, permite mostrar los datos con formato de Excell
+              hot_col(col = "categoria", type = "dropdown",allowInvalid = TRUE, #hot_col, da el formato a las columnas de nuestra tabla
+                      source = c("Comida", "Entretenimiento","Oficina", "Infraestructura","Transporte", "Compras", "Ropa", 
+                                 "Teléfono", "Salud", "Deporte","Electrónica","Cuidado personal","Mascota","Viajes","Educación", 
+                                 "Impuestos", "Automóvil","Estados de cuenta")     
+                      )
+                  )
+             )
+    } else{
+    rhandsontable(df, width = 800,height = 1000) %>%
+        hot_col(col = "categoria", type = "dropdown",allowInvalid = TRUE,
+                source = c("Comida", "Entretenimiento","Oficina", "Infraestructura","Transporte", "Compras", "Ropa", 
+                                                                "Teléfono", "Salud", "Deporte","Electrónica","Cuidado personal","Mascota","Viajes","Educación", 
+                                                                "Impuestos", "Automóvil","Estados de cuenta")
+                )
+      }
   })
-  #Guarda los cambios en la Tabla
+  #Guarda los cambios en la Tabla, de la ruta que tomamos el arhivo.
   observeEvent(input$Btn_Guardar,
-               write.csv(hot_to_r(input$Tabla),file = "iris.csv", row.names = FALSE))
+               write.csv(hot_to_r(df),file = "TDC.csv", row.names = FALSE))
 }
 
 shinyApp(ui, server)
