608e54572df824b8e6c5a4b15e6b9d74ae168e11
Corderodedios182 <corderodedios182@gmail.com>
2021-03-15 16:53:49 -0600

    Nuevas visualizaciones y Resumenes

--- gapminder/app.R
+++ gapminder/app.R
@@ -2,41 +2,46 @@ library(shiny)
 library(ggplot2)
 library(tidyverse)
 library(shinythemes)
+library(gapminder)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
     
-    titlePanel("Life Expectation vs. GDP Per Capita"),
-    theme = shinytheme("superhero"),
-    shinythemes::shinytheme("paper"),
+    titlePanel("Exploracion de Datos, base Gapminder"),
+    theme = shinytheme("yeti"),
+    #shinythemes::themeSelector(),
+    
     #3.Update Layout (UI)
     sidebarLayout(
         sidebarPanel(
-            #1.Add input (UI)
-            selectInput("continent","Select Continent", choices = unique(gapminder$continent)),
-            sliderInput("year", "Select Year", min = 1952, max = 2007, value = 1992, step = 5 )
-        ),
+    
+                    #1.Add input (UI)
+                    selectInput("continent","Select Continent", choices = unique(gapminder$continent)),
+                    sliderInput("year", "Select Year", min = 1952, max = 2007, value = 1992, step = 5 )),
+        
         mainPanel(    
             tabsetPanel(
-                #2.Add output (UI/Server)
-                tabPanel("Plot", plotOutput("plot")),
-                tabPanel("Table", DT::DTOutput('table'))
-                )
-            )
+                        #2.Add output (UI/Server)
+                        tabPanel("Plot", plotOutput("plot_1"), plotOutput("plot_2")),
+                        tabPanel("Table", DT::DTOutput('table')) ))
         )
     )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$plot <- renderPlot({
-        ggplot(gapminder %>% filter(continent == input$continent & year == input$year),
-               aes(x = gdpPercap, y = lifeExp)) + geom_point()})
-    output$table <- DT::renderDT({
-        gapminder %>% 
-            filter(continent == input$continent & year == input$year)
-    })
-
-}
+    
+    data_reactive <- reactive({ gapminder %>% filter(continent == input$continent & year == input$year) })
+    
+    output$plot_1 <- renderPlot({ ggplot(data_reactive()) + 
+                                    geom_point(mapping =aes(x=gdpPercap,y=lifeExp,color=country, size=pop)) })
+    
+    output$plot_2 <- renderPlot({ data_reactive() %>% arrange(desc(lifeExp)) %>%
+            ggplot(mapping = aes(x = country, y = lifeExp, fill = country)) +
+            geom_col(position = "dodge", show.legend = FALSE) + 
+            coord_flip() })
+    
+    output$table <- DT::renderDT({ data_reactive() %>% group_by(continent, year) %>% summarise(freq = n(),
+                                                                                               promedio = round(mean(lifeExp)))  })
+    
+    }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
